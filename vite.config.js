a5670a6f2c02e15269c5b51a21b116c3bb252ed0
yy <1744272885@qq.com>
2025-12-25 10:21:30 +0800

    删除多余图片 重命名图片文件 实现轮播图效果

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
-  },
-});
+    open: true
+  }
+})
