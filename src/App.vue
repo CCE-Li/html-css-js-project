433b6f1a95ed5213931db8a4dfc463ac54333e48
CCE-Li <2766503189@qq.com>
2025-12-25 10:51:35 +0800

    重配置router

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default defineComponent({
 <template>
   <div id="app">
     <router-view />
+    <TabBar />
   </div>
 </template>
 
@@ -21,6 +22,6 @@ export default defineComponent({
 :global(body) {
   margin: 0;
   font-family: 'HarmonyOS Sans', 'PingFang SC', 'Microsoft YaHei', sans-serif;
-  background: #f5f7fb;
+  background: #ffffff;
 }
 </style>
